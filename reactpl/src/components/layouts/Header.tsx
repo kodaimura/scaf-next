dc916c3d00409bd3860e3c10f975a605753b4539
kodaimura <kodaimura123@gmail.com>
2022-09-17 11:47:54 +0900

    add header navbar-burger

--- reactpl/src/components/layouts/Header.tsx
+++ reactpl/src/components/layouts/Header.tsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
 export const Header = (props: {
 	rightContent: React.ReactNode
 }) => {
+	const [isActive, setIsActive] = useState(false);
+
 
 	return (
 		<header className="navbar is-dark mb-3">
-		<div className="navbar-brand">
-			<a className="navbar-item" href="/">REACTPL</a>
-		</div>
-
-		<div className="navbar-end">
-			<div className="navbar-item">{props.rightContent}</div>
-		</div>
+			<div className="navbar-brand">
+				<a className="navbar-item is-size-4" href="/">REACTPL</a>
+				<a onClick={() => {setIsActive(!isActive);}} 
+					role="button"
+					className={`navbar-burger burger ${isActive ? "is-active" : ""}`}
+					data-target="navbarBurgers" 
+					aria-expanded="false"
+				>
+					<span aria-hidden="true"></span>
+					<span aria-hidden="true"></span>
+					<span aria-hidden="true"></span>
+				</a>
+			</div>
+			<div className={`navbar-menu ${isActive ? "is-active" : ""}`}
+				id="navbarBurgers" 
+			>
+				<div className="navbar-end">
+					<div className="navbar-item">{props.rightContent}</div>
+				</div>
+			</div>
 		</header>
 	);
 }
