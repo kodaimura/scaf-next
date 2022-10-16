318b687a845876d673713c66100f013cfb4f0a41
kodaimura <kodaimura123@gmail.com>
2022-10-16 20:24:30 +0900

    Header navbar-burger a->div

--- reactpl/src/components/layouts/Header.tsx
+++ reactpl/src/components/layouts/Header.tsx
@@ -11,8 +11,7 @@ export const Header = (props: {
 		<header className="navbar is-dark mb-3">
 			<div className="navbar-brand">
 				<a className="navbar-item is-size-4" href="/">REACTPL</a>
-				<a onClick={() => {setIsActive(!isActive);}} 
-					role="button"
+				<div onClick={() => {setIsActive(!isActive);}} 
 					className={`navbar-burger burger ${isActive ? "is-active" : ""}`}
 					data-target="navbarBurgers" 
 					aria-expanded="false"
@@ -20,7 +19,7 @@ export const Header = (props: {
 					<span aria-hidden="true"></span>
 					<span aria-hidden="true"></span>
 					<span aria-hidden="true"></span>
-				</a>
+				</div>
 			</div>
 			<div className={`navbar-menu ${isActive ? "is-active" : ""}`}
 				id="navbarBurgers" 
