f5c4a8f7ed13b5ea9efa1590e938aead92cf8811
kodaimura <kodaimura123@gmail.com>
2025-02-02 20:51:24 +0900

    add dashboard

---
+++ src/app/(auth)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React, { useState } from 'react';
+
+const DashboardPage: React.FC = () => {
+  return (
+    <div>
+      マイページ
+    </div>
+  );
+};
+
+export default DashboardPage;
