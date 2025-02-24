c905e8ed39652fee17546127ede6d62371040829
kodaimura <kodaimura123@gmail.com>
2025-02-24 20:20:21 +0900

    next.svg

--- src/frontend/src/app/page.tsx
+++ src/frontend/src/app/page.tsx
@@ -3,6 +3,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
+      <img src="next.svg"></img>
       <section className={styles.intro}>
         <h2>Getting Started</h2>
         <p>Next.js makes building web applications easy and efficient.</p>
