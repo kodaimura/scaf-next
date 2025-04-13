49d6aedcee822e8a094d761a39e668df38d58ff5
kodaimura <kodaimura123@gmail.com>
2025-04-14 00:11:24 +0900

    rm Image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import styles from "./page.module.css";
-import Image from 'next/image';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Image src="next.svg" alt="logo"></Image>
       <section className={styles.intro}>
         <h2>Getting Started</h2>
         <p>Next.js makes building web applications easy and efficient.</p>
